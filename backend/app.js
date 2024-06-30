4f9a877f59069b661342588fff299945d1ee9dc7
sudofoce <paolo.dealca@icloud.com>
2024-06-30 16:22:38 +0100

    uncomment env production

--- backend/app.js
+++ backend/app.js
@@ -17,9 +17,9 @@ process.on("uncaughtException", (err) => {
   process.exit(1);
 });
 
-// if (process.env.NODE_ENV !== "PRODUCTION") {
-dotenv.config({ path: "backend/config/config.env" });
-// }
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  dotenv.config({ path: "backend/config/config.env" });
+}
 
 // Connecting to Database
 connectDatabase();
