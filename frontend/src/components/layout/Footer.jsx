e7a435e9a0982eefa73b1e5af4d3a9dfdf6c3c25
sudofoce <paolo.dealca@icloud.com>
2024-07-25 16:12:57 +0100

    removed year at footer jsx

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -3,9 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="py-1 pt-5">
-      <p className="text-center mt-1 fw-bold">
-        ShopIT - 2024, All Rights Reserved
-      </p>
+      <p className="text-center mt-1 fw-bold">ShopIT - All Rights Reserved</p>
     </footer>
   );
 };
