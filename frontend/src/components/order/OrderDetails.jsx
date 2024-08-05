7580d8d63bc2b3ec246c8fe17ca0419c6c6a9be8
sudofoce <paolo.dealca@icloud.com>
2024-08-05 22:27:39 +0100

    changed the link url order id

--- frontend/src/components/order/OrderDetails.jsx
+++ frontend/src/components/order/OrderDetails.jsx
@@ -37,9 +37,15 @@ const OrderDetails = () => {
         <div className="col-12 col-lg-9 mt-5 order-details">
           <div className="d-flex justify-content-between align-items-center">
             <h3 className="mt-5 mb-4">Your Order Details</h3>
-            <a className="btn btn-success" href="/invoice/order/order-id">
+            {/* <a className="btn btn-success" href="/invoice/order/order-id">
               <i className="fa fa-print"></i> Invoice
-            </a>
+            </a> */}
+            <Link
+              className="btn btn-success"
+              to={`/invoice/order/${order?._id}`}
+            >
+              <i className="fa fa-print"></i> Invoice
+            </Link>
           </div>
           <table className="table table-striped table-bordered">
             <tbody>
